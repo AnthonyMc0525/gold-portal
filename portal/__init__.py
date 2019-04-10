e5ab2968d7ceb53e86cdfb07bbaef03837f3cd83
Zach Lehman <zalehman182@stevenscollege.edu>
2019-04-10 15:20:42 -0400

    Add backend to login
    - For post checked user against name in database

--- portal/__init__.py
+++ portal/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 
 def create_app(test_config=None):
@@ -21,6 +21,20 @@ def create_app(test_config=None):
     @app.route('/', methods=('GET', 'POST'))
     def index():
         method = request.method
+        if method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            con = db.get_db()
+            cur = con.cursor()
+            cur.execute("SELECT * FROM users WHERE email=%s", (email,))
+            user = cur.fetchone()
+            if user == None:
+                flash('Your Email was Incorrect')
+            elif user[2] != password:
+                flash('Your Password was Incorrect')
+            cur.close()
+            con.close()
         return render_template('index.html', method=method)
+
 
     return app
