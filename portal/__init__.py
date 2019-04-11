e8db8a10946f5038d5437b7b37252ab1678c96b2
Anthony McMellen <anthony@BHB-CSET-18-13.local>
2019-04-11 15:01:11 -0400

    created check for correct user

--- portal/__init__.py
+++ portal/__init__.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, request, session, flash
 
 
 def create_app(test_config=None):
@@ -15,6 +15,8 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DB_NAME='portal',
         DB_USER='portal_user',
+        EMAIL='teacher@stevenscollege.edu',
+        PASSWORD='qwerty',
     )
 
     if test_config is None:
@@ -27,21 +29,31 @@ def create_app(test_config=None):
 
     @app.route('/', methods=('GET', 'POST'))
     def index():
+        logged_in = False
         method = request.method
+        print(method)
         if method == 'POST':
             email = request.form['email']
             password = request.form['password']
             con = db.get_db()
             cur = con.cursor()
+            error = None
             cur.execute("SELECT * FROM users WHERE email=%s", (email,))
             user = cur.fetchone()
-            if user == None:
-                flash('Your Email was Incorrect')
-            elif user[2] != password:
-                flash('Your Password was Incorrect')
+            if user is None:
+                error = 'Incorrect error'
+            elif user[4] != password:
+                error = 'Your Password was Incorrect'
+
+            if error is None:
+                logged_in = True
+                session.clear()
+                session['user_id'] = user[0]
+
             cur.close()
             con.close()
-        return render_template('index.html', method=method)
+
+        return render_template('index.html', logged_in=logged_in)
 
 
     return app
