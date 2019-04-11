28fbc2050eafd9687a51f010c0dc16683ba69f9d
Jason Wyatt <stryfecrab22@gmail.com>
2019-04-11 13:12:10 -0400

    WIP commit, everything should work

--- portal/__init__.py
+++ portal/__init__.py
@@ -26,4 +26,30 @@ def create_app(test_config=None):
     def index():
         return render_template('index.html')
 
+    @app.route('/create-course', methods=['GET', 'POST'])
+    def course():
+        if request.method == "GET":
+            return render_template('create-course.html')
+
+        elif request.method == "POST":
+            new_course = request.form['']
+
+            if new_course:
+
+                # Save to database
+                con = db.get_db()
+                cur = con.cursor()
+                cur.execute(
+                        "INSERT INTO courses (course, course_id, teacher) VALUES (%s, %s, %s)",
+                        (new_course, course_id, teacher)
+                )
+                con.commit()
+                cur.close()
+
+                flash('Your new course is now created. Want to add another?', 'success')
+            else:
+                flash('You need to add some information first.', 'error')
+
+        return render_template('create-course.html', new_course=new_course)
+
     return app
--- portal/schema.sql
+++ portal/schema.sql
@@ -1,8 +1,16 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS courses;
 
 CREATE TABLE users (
     id bigserial PRIMARY KEY,
     email text UNIQUE NOT NULL,
     password text NOT NULL,
     role varchar(7) NOT NULL CHECK (role IN ('teacher', 'student'))
+);
+
+CREATE TABLE courses (
+    id bigserial PRIMARY KEY,
+    course text NOT NULL,
+    course_id text UNIQUE NOT NULL,
+    teacher text
 );
